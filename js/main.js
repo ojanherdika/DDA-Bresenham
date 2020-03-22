5177fe3fff40b37b31acfbc3fa335f286cc4d95a
zanuarts <nuartea27@gmail.com>
2020-03-22 09:48:45 +0700

    add a little rumus

--- js/main.js
+++ js/main.js
@@ -1,12 +1,42 @@
-function print(s) { document.getElementById('out').innerHTML += s; }
-function println(s) { document.getElementById('out').innerHTML += s + '\n'; }
+function dda(){
+  var x1 = document.getElementById("x1").value;
+  var y1 = document.getElementById("y1").value;
+  var x2 = document.getElementById("x2").value;
+  var y2 = document.getElementById("y2").value;
+  
+  dx = x2 - x1;
+  dy = y2 - y1;
 
-var size = 5,
-    i, j;
+  if (dx > dy) {
+      steps = Math.abs(dx);
+  } else {
+      steps = Math.abs(dy);
+  }
 
-for (i = 0; i < size; i++) {
-    for (j = 0; j < size; j++) {
-        print("*");
-    }
-    println("");
+  xIncr = dx / steps;
+  yIncr = dy / steps;
 }
+function tableCreate() {
+  var body = document.getElementsByTagName('body')[0];
+  var tbl = document.createElement('table');
+  tbl.style.width = '100%';
+  tbl.setAttribute('border', '1');
+  var tbdy = document.createElement('tbody');
+  for (var i = 0; i < 3; i++) {
+    var tr = document.createElement('tr');
+    for (var j = 0; j < 2; j++) {
+      if (i == 2 && j == 1) {
+        break
+      } else {
+        var td = document.createElement('td');
+        td.appendChild(document.createTextNode('\u0020'))
+        i == 1 && j == 1 ? td.setAttribute('rowSpan', '2') : null;
+        tr.appendChild(td)
+      }
+    }
+    tbdy.appendChild(tr);
+  }
+  tbl.appendChild(tbdy);
+  body.appendChild(tbl)
+}
+tableCreate();
