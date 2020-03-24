d9fc96818ffa5f547784cd21b63ae85da76ac09f
zanuarts <nuartea27@gmail.com>
2020-03-24 10:30:49 +0700

    add table not fix

--- js/main.js
+++ js/main.js
@@ -16,6 +16,11 @@ function dda(){
   xIncr = dx / steps;
   yIncr = dy / steps;
 
+  var i,
+  for (i=0; i<x1; i++){
+
+  }
+
 }
 
 function bressenham(){
@@ -27,27 +32,16 @@ function bressenham(){
 
 }
 
-function tableCreate() {
-  var body = document.getElementsByTagName('body')[0];
-  var tbl = document.createElement('table');
-  tbl.style.width = '100%';
-  tbl.setAttribute('border', '1');
-  var tbdy = document.createElement('tbody');
-  for (var i = 0; i < 3; i++) {
-    var tr = document.createElement('tr');
-    for (var j = 0; j < 2; j++) {
-      if (i == 2 && j == 1) {
-        break
-      } else {
-        var td = document.createElement('td');
-        td.appendChild(document.createTextNode('\u0020'))
-        i == 1 && j == 1 ? td.setAttribute('rowSpan', '2') : null;
-        tr.appendChild(td)
-      }
+
+
+let container = document.querySelector('.table-container')
+let tableContent = "<table>"
+for (let row=ySize; row>=0; row--) {
+    tableContent += "<tr>";
+    for(let col=0; col<=xSize; col++) {
+        tableContent += `<td class="pixel" id="${col},${row}" onclick="select(${col}, ${row})"></td>`;
     }
-    tbdy.appendChild(tr);
-  }
-  tbl.appendChild(tbdy);
-  body.appendChild(tbl)
+    tableContent += "</tr>";
 }
-tableCreate();
+tableContent += "</table>";
+container.innerHTML += tableContent;
